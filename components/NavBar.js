d784310dba28980e3b92a62449f953fae2775512
mahmoudGhammad <mahmoud.g.hammad@gmail.com>
2022-11-23 16:16:58 +0200

    03

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,14 +7,14 @@ function NavBar() {
     <>
     <div className='flex flex-row-reverse justify-between shadow-lg  '>
     <Link href='/'>
-        <div className='text-blue-700 hover:cursor-pointer flex flex-col mr-10 mt-10'>
+        <div className='text-blue-700 hover:cursor-pointer flex flex-col mr-5 mt-10'>
         
         <HomeIcon className=' h-10 w-10 hover:animate-bounce self-center '/>
         <p className=' font-extrabold text-2xl'> الرئيسية</p>
         </div>
       </Link>
       <Link href='/claim'>
-        <div className='text-blue-700 hover:cursor-pointer flex flex-col mr-10 mt-10 '>
+        <div className='text-blue-700 hover:cursor-pointer flex flex-col  mt-10 '>
           <MegaphoneIcon className=' h-10 w-10 animate-pulse hover:animate-bounce self-center '/>
         <p className=' font-extrabold text-2xl'>الشكاوي</p>
         </div>
@@ -26,7 +26,7 @@ function NavBar() {
         </div>
       </Link>
 
-      <img src='Pharos.jpg' className=' h-28 mt-3 mb-3 ml-10'></img>
+      <img src='Pharos.jpg' className=' h-28 mt-3 mb-3 ml-5'></img>
     </div>
       
     </>
