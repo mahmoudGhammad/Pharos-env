f2eb14ce1712f6aea5101f58bc5a9fe752f1286e
mahmoudGhammad <mahmoud.g.hammad@gmail.com>
2022-11-23 16:29:10 +0200

    04

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,28 +5,28 @@ import { HomeIcon , BanknotesIcon  , MegaphoneIcon} from '@heroicons/react/24/ou
 function NavBar() { 
   return (
     <>
-    <div className='flex flex-row-reverse justify-between shadow-lg  '>
+    <div className='flex justify-between shadow-lg flex-row-reverse '>
     <Link href='/'>
         <div className='text-blue-700 hover:cursor-pointer flex flex-col mr-5 mt-10'>
         
-        <HomeIcon className=' h-10 w-10 hover:animate-bounce self-center '/>
-        <p className=' font-extrabold text-2xl'> الرئيسية</p>
+        <HomeIcon className=' h-6 w-6 hover:animate-bounce self-center '/>
+        <p className=' font-extrabold text-l'> الرئيسية</p>
         </div>
       </Link>
       <Link href='/claim'>
         <div className='text-blue-700 hover:cursor-pointer flex flex-col  mt-10 '>
-          <MegaphoneIcon className=' h-10 w-10 animate-pulse hover:animate-bounce self-center '/>
-        <p className=' font-extrabold text-2xl'>الشكاوي</p>
+          <MegaphoneIcon className=' h-6 w-6 animate-pulse hover:animate-bounce self-center '/>
+        <p className=' font-extrabold text-l'>الشكاوي</p>
         </div>
       </Link>
       <Link href='/exchange'>
         <div className='text-blue-700 hover:cursor-pointer flex flex-col  mt-10'>
-          <BanknotesIcon className=' h-10 w-10 hover:animate-bounce self-center '/>
-        <p className='font-extrabold text-2xl'>مبادلة</p>
+          <BanknotesIcon className=' h-6 w-6 hover:animate-bounce self-center '/>
+        <p className='font-extrabold text-l'>مبادلة</p>
         </div>
       </Link>
 
-      <img src='Pharos.jpg' className=' h-28 mt-3 mb-3 ml-5'></img>
+      <img src='Pharos.jpg' className=' h-20 w-20 mt-3 mb-3 ml-5'></img>
     </div>
       
     </>
